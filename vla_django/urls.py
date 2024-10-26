ca3db5faedc0327d729b46baeeece0fd8a060e4b
Matthieu PEREZ <matthieu.perez30@gmail.com>
2024-10-26 20:58:20 +0200

    vcm prgm test

--- vla_django/urls.py
+++ vla_django/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import index
 
+# from vcm.models import Vcm
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
     path("infos/", include("infos.urls")),
     path("vcm/", include("vcm.urls")),
+    # path("vcm/<str:slug>/", vcm, name="vcm"),
 ]
