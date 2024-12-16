fa4a82473cf300470b475387a4c20203396fee38
Matthieu PEREZ <matthieu.perez30@gmail.com>
2024-12-16 20:09:09 +0100

    private page going on

--- private/views.py
+++ private/views.py
@@ -2,24 +2,36 @@ from django.shortcuts import redirect, render
 from django.views import View
 from persons.models import Person
 from notes.models import Note
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 def is_connected(user):
     return user.is_authenticated
 
 
+@method_decorator(login_required, name="dispatch")
 class PrivateView(View):
     def get(self, request):
         user = request.user
-        person = Person.objects.get(firstname=user.first_name, lastname=user.last_name)
-        persons = Person.objects.all()
-        notes = Note.objects.filter(writer_id=person.id)
+        
+        if is_connected(user):
+            user = request.user
+            person = Person.objects.get(
+                firstname=user.first_name, lastname=user.last_name
+            )
+            persons = Person.objects.all()
+            notes = Note.objects.filter(writer_id=person.id)
 
-        return render(
-            request,
-            "private/index.html",
-            {
+            context = {
                 "persons": persons,
                 "notes": notes,
-            },
-        )
+            }
+
+            return render(
+                request,
+                "private/index.html",
+                context,
+            )
+        else:
+            return redirect("signup")
