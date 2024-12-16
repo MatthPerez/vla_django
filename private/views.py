8f577fafd76f4753f2223c3b7657eaee03777bbd
Matthieu PEREZ <matthieu.perez30@gmail.com>
2024-12-16 19:36:15 +0100

    private note crud

--- private/views.py
+++ private/views.py
@@ -10,8 +10,10 @@ def is_connected(user):
 
 class PrivateView(View):
     def get(self, request):
+        user = request.user
+        person = Person.objects.get(firstname=user.first_name, lastname=user.last_name)
         persons = Person.objects.all()
-        notes = Note.objects.all()
+        notes = Note.objects.filter(writer_id=person.id)
 
         return render(
             request,
@@ -21,5 +23,3 @@ class PrivateView(View):
                 "notes": notes,
             },
         )
-
-
