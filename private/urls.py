924eaf3df2082fb6e5803b732c68bb05cb81cc18
Matthieu PEREZ <matthieu.perez30@gmail.com>
2025-01-02 18:37:23 +0100

    fix private urls, communications style

--- private/urls.py
+++ private/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import PrivateView
 
 urlpatterns = [
-    path("ma_page/", PrivateView.as_view(), name="private"),
-    # path("ma_page/note/", NewNote.as_view(), name="note_new"),
-    # path("ma_page/note/<int:pk>/modifier", NoteUpdate.as_view(), name="note_update"),
-    # path("ma_page/note/<int:pk>/supprimer", NoteDelete.as_view(), name="note_delete"),
+    path("", PrivateView.as_view(), name="private"),
+    # path("note/", NewNote.as_view(), name="note_new"),
+    # path("note/<int:pk>/modifier", NoteUpdate.as_view(), name="note_update"),
+    # path("note/<int:pk>/supprimer", NoteDelete.as_view(), name="note_delete"),
 ]
